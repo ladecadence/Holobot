a06bddbff24167fd755b508b90a5f13d15f8fd3c
David Pello <dvpg@tuta.io>
2022-05-31 12:44:10 +0200

    added pause time and full path to pictures

--- src/config.py
+++ src/config.py
@@ -3,12 +3,12 @@ options = {
     "ip_robot" : "192.168.1.3",
     "port_robot" : 7000,
     "image_path" : "/home/zako/Imáxenes/",
-    "ip_vis" : "192.168.10.12",
-    "port_vis" : 1234,
+    "pause" : 10,
+    "ip_vis" : "192.168.43.44",
+    "port_vis" : 9000,
     "brightness" : "50",
-    "picture_path" : "../img/",
+    "picture_path" : "/home/zako/Ladecadence/Trabajo/Hiriki/Expos/Holobot/Holobot/img/",
     "picture_name" : "img.bmp",
     "temp_picture_name" : "temp.bmp",
     "http_server_port" : 8000,
-
  }

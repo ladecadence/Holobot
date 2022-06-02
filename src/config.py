ab75afa9b1918a44cb95ae018dbbb7f4e32164d9
David Pello <dvpg@tuta.io>
2022-06-02 10:42:58 +0200

    Cleanup and reorder

--- src/config.py
+++ src/config.py
@@ -1,14 +1,19 @@
 options = {
+    # IPs and ports
     "url_leds": "http://holobot-leds.local",
     "ip_robot" : "192.168.1.3",
     "port_robot" : 7000,
-    "image_path" : "/home/zako/Imáxenes/",
-    "pause" : 10,
-    "ip_vis" : "192.168.43.44",
+    "ip_vis" : "192.168.188.44",
     "port_vis" : 9000,
+    "http_server_port" : 8000,
+    # config options
+    "pause" : 10,
+    "test_movement" : 0,
     "brightness" : "50",
+    "default_aperture" : "25"
+    # paths
+    "image_path" : "/home/zako/Imáxenes/",
     "picture_path" : "/home/zako/Ladecadence/Trabajo/Hiriki/Expos/Holobot/Holobot/img/",
     "picture_name" : "img.bmp",
     "temp_picture_name" : "temp.bmp",
-    "http_server_port" : 8000,
  }
